fd5dbe357b90bdb137236b89e3a99a9bc2fe4dae
AdhilKappil <adhilalikappil@gmail.com>
2024-05-08 20:26:53 +0530

    feat:Socket add conection and disconection

--- src/infrastructureLayer/services/Socket.ts
+++ src/infrastructureLayer/services/Socket.ts
@@ -1,32 +1,55 @@
 import { Server, Socket } from "socket.io";
 import { createServer as createHttpServer, Server as HttpServer } from "http";
 
-export class SocketManager {
-  private httpServer: HttpServer;
-  private io: Server;
-//   private readonly userRepository: UserRepository;
-
-  constructor(httpServer: HttpServer) {
-    // this.userRepository = userRepository;
-    this.httpServer = httpServer;
-    this.io = new Server(httpServer, {
-      cors: {
-        origin:"http://localhost:9000",
-      }
-    //   path: "/socket-chat/",
-    });
-
-    this.io.on("connection", this.handleConnection);
-  }
-
-  //to create a room of all the students with particular classRoomId
-  private handleConnection = (socket: Socket): void => {
-    
-    this.io.on("connection",(socket)=>{
-        console.log("a user connected.");
-        this.io.emit("welcome", "hello this is socket server!")
-    })
-  
-  };
-
+interface IUser {
+    userId: string;
+    socketId: string;
 }
+
+export class SocketManager {
+    private httpServer: HttpServer;
+    private io: Server;
+    private users: IUser[] = [];
+  
+    constructor(httpServer: HttpServer) {
+        this.httpServer = httpServer;
+        this.io = new Server(httpServer, { 
+            cors: {
+                origin: "http://localhost:9000",
+            },
+        });
+
+        this.io.on("connection", this.handleConnection);
+    }
+
+    private handleConnection = (socket: Socket): void => {
+        // when user connect
+        console.log("a user connected.");
+
+        // add user id and socket id 
+        socket.on("addUser", (userId: string) => {
+            this.addUser(userId, socket.id);
+        });
+
+        // when user disconnect
+        socket.on("disconnect", () => {
+            console.log("a user disconnected!");
+            this.removeUser(socket.id);
+            this.io.emit("getUsers", this.users);
+        });
+    };
+
+    // add user id and socket id 
+     addUser(userId: string, socketId: string): void {
+        if (!this.users.some((user) => user.userId === userId)) {
+            this.users.push({ userId, socketId });
+            this.io.emit("getUsers", this.users);
+        }
+    }
+
+    // remove user here
+     removeUser(socketId: string): void {
+        this.users = this.users.filter((user) => user.socketId !== socketId);
+    }
+}
+ 
